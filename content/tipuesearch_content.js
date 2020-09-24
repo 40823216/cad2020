e600afca111b65a15df40197641904c75e98a7bd
40823216 <40823216@gm.nfu.edu.tw>
2020-09-24 23:17:15 +0800

    40823216 cad2020

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': '每周進度', 'text': '', 'tags': '', 'url': '每周進度.html'}, {'title': 'w1', 'text': '', 'tags': '', 'url': 'w1.html'}]};
