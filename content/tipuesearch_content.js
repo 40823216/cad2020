cabc65a7d1ebe3cd147968c92e9c72696684f212
40823216 <40823216@gm.nfu.edu.tw>
2020-10-30 12:10:38 +0800

    w1 pdf1-5

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'Work', 'text': '', 'tags': '', 'url': 'Work.html'}, {'title': 'w1', 'text': '使用軟體為Inventer2019學生版 \n pdf1-1 \n \n', 'tags': '', 'url': 'w1.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'Work', 'text': '', 'tags': '', 'url': 'Work.html'}, {'title': 'w1', 'text': '使用軟體為Inventer2019學生版 \n pdf1-1 \n \n pdf1-2 \n \n pdf1-3 \n \n pdf1-4 \n \n pdf1-5 \n', 'tags': '', 'url': 'w1.html'}]};
